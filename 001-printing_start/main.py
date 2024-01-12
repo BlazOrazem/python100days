a96cccbd011c9b8011b5d8a4f793598ad7f24a84
Blaž Oražem <blaz@orazem.info>
2024-01-12 21:57:43 +0100

    Section 1: Day 1 - Beginner - Working with Variables in Python to Manage Data

---
+++ 001-printing_start/main.py
@@ -0,0 +1,29 @@
+# Printing to the Console
+print("Hello world!")
+
+# Breaking lines
+print("Hello world!\nHello world!")
+
+# Concatenating variables
+print("Hello" + " " + "Joe")
+
+# Input Function
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?"))
+
+# Python Variables
+name = input("What is your name?")
+print(name)
+length = len(name)
+print(length)
+
+# Variable Naming
+user_name = "Joe"
+print(user_name)
+
+# Day 1 Project: Band Name Generator
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
